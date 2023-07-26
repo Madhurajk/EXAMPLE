cd22ea686df193fe508f0e51792f9496d3040962
Madhurajk <madhuraj.kumara@lucidatechnologies.com>
2023-07-26 14:06:13 +0530

    test

--- src/components/SignIn/__LoginTest__/LoginTest.test.js
+++ src/components/SignIn/__LoginTest__/LoginTest.test.js
@@ -1,13 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import SignIn from '../SignIn'
 
-describe("testing Login Components" ,()=>{
+ describe("testing Login Components" ,()=>{
     test("test redering of Login Form correctly", ()=>{
-
-        // const SignInComponent =
          render(<SignIn />)
         const emailField = screen.getByText(/Email :/)
-        const passwordFieled = screen.getByText(/Password :/)
         expect(emailField);
-    })
+    }),
+    test("test redering of Login Form correctly", ()=>{
+         render(<SignIn />)
+        const passwordFieled = screen.getByText(/Password :/)
+        expect(passwordFieled);
+    }),
+    test("test redering of Email id is correctly", ()=>{
+       const SignupPage = render(<SignIn />)
+      const buttons  =  SignupPage.findAllByRole("button")
+      expect(buttons)
+   })
+
 })
