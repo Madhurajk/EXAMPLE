d6a973c0ed080df250850234432bc67902919fc5
Madhurajk <madhuraj.kumara@lucidatechnologies.com>
2023-07-26 14:39:39 +0530

    test

--- src/components/SignIn/__LoginTest__/LoginTest.test.js
+++ src/components/SignIn/__LoginTest__/LoginTest.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import SignIn from '../SignIn'
-
  describe("testing Login Components" ,()=>{
     test("test redering of Login Form correctly", ()=>{
          render(<SignIn />)
@@ -16,11 +15,17 @@ import SignIn from '../SignIn'
        const SignupPage = render(<SignIn />)
       const buttons  =  SignupPage.findAllByRole("button")
       expect(buttons)
-   })
+   }),
 test("valiadate email id should be correctly",()=>{
     render(<SignIn />)
     const validEmailInput = "Madhu@gmail.com";
-    except(validateEmail(validEmailInput)).toBe(true)
+    except(validateEmail(validEmailInput)).toBe(false)
+}),
+test("Email id should run correctly", ()=>{
+render(<SignIn />)
+const msg = screen.getByRole(/Don't have an account? Sign Up/);
+// userEvent.type(email, "madhu@gmail.com")
+except(msg)
 })
 
 })
